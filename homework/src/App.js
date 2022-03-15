72a4c66dd9274096b05c8567dfc3e6934b14b869
vilenani <vilena.niyaz@gmail.com>
2022-03-15 20:44:51 +0500

    hw1

---
+++ homework/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Message from './components/Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message text='Walking, running, cycling and playing football are some kinds of sports that you do every day. Some people think that doing sports is useless, the other consider that it is tiring. So, why is it so important to do sports?
+When you move, 600 muscles in your body work to make you more flexible and stronger. Sports are very important for children and teenagers. When their bones are well held by the muscles, they grow and become stronger.' />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ homework/src/components/Message/index.js
@@ -0,0 +1,13 @@
+
+const Message = (props) => {
+    return (<div>
+        <p>{props.text}</p>
+    </div>
+
+    )
+}
+
+
+
+
+export default Message
